abe789436487f639600708d9b4e5f86f1fc23bf6
Raiteia <raiteia8943@gmail.com>
2018-04-28 01:50:38 +0800

    Initial

---
+++ Plugins/MOBA/Source/MOBA/Private/HeroBuff.cpp
@@ -0,0 +1,157 @@
+﻿ // Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MOBAPrivatePCH.h"
+#include "HeroBuff.h"
+#include "UnrealNetwork.h"
+#include "HeroCharacter.h"
+#include "MOBAPlayerController.h"
+
+AHeroBuff::AHeroBuff(const FObjectInitializer& ObjectInitializer)
+	: Super(FObjectInitializer::Get())
+{
+	bReplicates = true;
+	PrimaryActorTick.bCanEverTick = true;
+	Interval = 0;
+	IntervalCounting = 0;
+	IntervalCount = 0;
+	Particle = ObjectInitializer.CreateDefaultSubobject<UParticleSystemComponent>(this, TEXT("Particle0"));
+	Particle->SetupAttachment(RootComponent);
+}
+
+AHeroBuff* AHeroBuff::NewHeroBuff()
+{
+	return NewObject<AHeroBuff>();
+}
+
+TArray<AHeroBuff*> AHeroBuff::CloneArray(TArray<AHeroBuff*> input)
+{
+	TArray<AHeroBuff*> res;
+	for (AHeroBuff* hb : input)
+	{
+		res.Add(hb->Clone());
+	}
+	return res;
+}
+
+AHeroBuff* AHeroBuff::Clone()
+{
+	AHeroBuff* data = NewObject<AHeroBuff>();
+	data->Priority = Priority;
+	data->Name = Name;
+	data->Head = Head;
+	data->BuffState = BuffState;
+	data->CanStacks = CanStacks;
+	data->Stacks = Stacks;
+	data->Duration = Duration;
+	return data;
+}
+
+void AHeroBuff::BeginPlay()
+{
+	Super::BeginPlay();
+	MaxDuration = Duration;
+	if (Forever)
+	{
+		Duration = 1;
+		ParticleDuration = 1;
+		RealDuration = 1;
+	}
+}
+
+void AHeroBuff::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (!Forever)
+	{
+		Duration -= DeltaTime;
+		ParticleDuration -= DeltaTime;
+		RealDuration -= DeltaTime;
+	}
+	if (ParticleDuration <= 0)
+	{
+		Particle->Deactivate();
+	}
+	if (RealDuration <= 0 && !IsPendingKillPending())
+	{
+		this->Destroy();
+	}
+	if (Interval > 0 && Duration >= 0)
+	{	
+		IntervalCounting += DeltaTime;
+		if (IntervalCounting >= Interval)
+		{
+			IntervalCount++;
+			IntervalCounting = 0;
+			OnInterval(IntervalCount);
+		}
+	}
+
+	if (Duration > 0)
+	{
+		AuraCount += DeltaTime;
+		if (AuraCount > 0.1)
+		{
+			AuraCount = 0;
+			TSet<AHeroCharacter*> tmp;
+			if (BuffUniqueMap.Contains(HEROU::AuraRadiusEnemy))
+			{
+				float range = BuffUniqueMap[HEROU::AuraRadiusEnemy];
+				TArray<AHeroCharacter*> Enemys = AHeroCharacter::localPC->FindRadiusActorByLocation(
+					BuffTargetOne, GetActorLocation(), range, ETeamFlag::TeamEnemy, true);
+				for (AHeroCharacter* EachHero : Enemys)
+				{
+					tmp.Add(EachHero);
+				}
+			}
+			if (BuffUniqueMap.Contains(HEROU::AuraRadiusFriends))
+			{
+				float range = BuffUniqueMap[HEROU::AuraRadiusFriends];
+				TArray<AHeroCharacter*> Enemys = AHeroCharacter::localPC->FindRadiusActorByLocation(
+					BuffTargetOne, GetActorLocation(), range, ETeamFlag::TeamFriends, true);
+				for (AHeroCharacter* EachHero : Enemys)
+				{
+					tmp.Add(EachHero);
+				}
+			}
+
+			// 拿到要得到光環的Actor
+			TSet<AHeroCharacter*> Result1;
+			Result1.Reserve(BuffTarget.Num()); // Worst case is no elements of this are in Other
+			for (AHeroCharacter* hero : tmp)
+			{
+				if (!BuffTarget.Contains(hero))
+				{
+					//Result1.Add(hero);
+					hero->AddUniqueBuff(this);
+					UParticleSystemComponent* CurrentEmitter = UGameplayStatics::SpawnEmitterAtLocation(
+						GetWorld(),
+						ParticleFX1,
+						hero->GetActorLocation(),
+						FRotator::ZeroRotator,
+						true);
+				}
+			}
+
+			// 拿到要被刪除光環的Actor
+			TSet<AHeroCharacter*> Result2;
+			Result2.Reserve(BuffTarget.Num()); // Worst case is no elements of this are in Other
+			for (AHeroCharacter* hero : BuffTarget)
+			{
+				if (!tmp.Contains(hero))
+				{
+					//Result2.Add(hero);
+					hero->RemoveBuff(this);
+				}
+			}
+			BuffTarget = tmp;
+		}
+	}
+}
+
+
+void AHeroBuff::GetLifetimeReplicatedProps(TArray< FLifetimeProperty >& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AHeroBuff, BuffTarget);
+	DOREPLIFETIME(AHeroBuff, BuffTargetOne);
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+<<<<<<< HEAD
+# AON
+- Remake Ambition Of Nobunaga
+- 重製信長 從小型連線RPG做起
+- 支持我們 https://www.patreon.com/nobu_game
+
+# License
+- for unreal part, you need follow unreal eula https://www.unrealengine.com/eula
+- for our part, use BSD https://zh.wikipedia.org/wiki/BSD%E8%AE%B8%E5%8F%AF%E8%AF%81
+
+# 怎麼使用 How to use ?
+
+## Install:
+- unreal 4.19  https://www.unrealengine.com/download
+- visual studio 2017 https://imagine.microsoft.com/zh-tw/Catalog/Product/530
+- git  https://git-scm.com/download/win
+- git lfs   https://git-lfs.github.com/
+- get github account  https://github.com/
+
+並把把顯卡升級到gtx 1050以上
+- install UE4.19.1 https://www.unrealengine.com/en-US/what-is-unreal-engine-4
+- install Visual Studio 2017
+- install git & git lfs
+- git clone https://github.com/RemakeAONTeam/AON.git
+- git lfs fetch
+- git lfs pull
+- download 3rd party from https://github.com/RemakeAONTeam/AON/releases Decompress to "C:\Program Files\Epic Games\UE_4.19\Engine\Source\ThirdParty"
+- mouse right click AON.uproject=>Switch Unreal Engine Version choose "4.19"
+- Open "AON.sln" and enter "Ctrl+F5" in VS2017
+
+# 怎麼更新 How to update ?
+- git pull
+- git lfs fetch
+- git lfs pull
+=======
+# GitTest
+>>>>>>> Initial commit
